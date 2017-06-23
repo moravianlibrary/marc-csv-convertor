b638310dbb12b88c3b627efa18014182bffa137a
SergeyPanov <panovsy@gmail.com>
2017-06-23 14:08:46 +0200

    Added statistics counter.

--- FilesManager/FilesManager.py
+++ FilesManager/FilesManager.py
@@ -3,17 +3,17 @@ import sys
 import re
 import csv
 import configparser
+import subprocess
 
-
-def check_file(pathToFile, extension):
-    if os.path.exists(pathToFile):
-        if pathToFile.endswith(extension):
+def check_file(path_to_file, extension):
+    if os.path.exists(path_to_file):
+        if path_to_file.endswith(extension):
             return True
         else:
             print("File has wrong extension, expected {}".format(extension), file=sys.stderr)
             raise IOError
     else:
-        print("File {} is not found".format(pathToFile), file=sys.stderr)
+        print("File {} is not found".format(path_to_file), file=sys.stderr)
         raise FileNotFoundError
 
 
@@ -22,10 +22,10 @@ class FilesManager:
     Class is managing input and output files.
     """
 
-    def __init__(self, input_file, output_file, config_file, separator):
+    def __init__(self, input_file, output_file, config_file, separator, step):
         check_file(input_file, ".mrc")
         check_file(config_file, ".ini")
-
+        self.step = step
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
         self.fields = dict(self.config["FIELDS"].items())
@@ -33,11 +33,8 @@ class FilesManager:
         self.output_file = output_file
         self.separator = separator
 
-
-
-    def process_record(self, record, o_file):
+    def process_record(self, record):
         processed = {}
-        clear_lambda = lambda lin: re.sub("\$\S?", "", lin)
         for line in record:
             line = re.sub("\n", "", line)
             field = ""
@@ -48,8 +45,9 @@ class FilesManager:
 
                     for subfield_tag in str(self.fields[field]).split():
 
-                        vals = list(map(lambda l: re.sub("\$\S?", "", l), list(filter(lambda s: len(s) > 0, list(map(lambda subfield: subfield if str(subfield).startswith(subfield_tag) else "",
-                                 re.findall("\$\S?[^\$\n]*", line)))))))
+                        vals = list(map(lambda l: re.sub("\$\S?", "", l), list(filter(lambda s: len(s) > 0, list(
+                            map(lambda subfield: subfield if str(subfield).startswith(subfield_tag) else "",
+                                re.findall("\$\S?[^\$\n]*", line)))))))
 
                         if field in processed:
                             processed[field] += vals
@@ -65,6 +63,10 @@ class FilesManager:
         return merged_fields
 
     def convert(self):
+        print("Counting records.")
+        amount_of_records = int(subprocess.check_output('grep {} -e "^LEADER" | wc -l'.format(self.input_file), stderr=subprocess.STDOUT, shell=True))
+        counter = 0
+        processed = 0
         with open(self.input_file) as iFile:
 
             with open(self.output_file, "w+") as o_file:
@@ -76,10 +78,19 @@ class FilesManager:
                 for line in iFile:
 
                     if line.startswith("LEADER") and len(marc_record) > 0:
-                        csv_writer.writerow(self.joiner(self.process_record(record=marc_record, o_file=o_file)))
+                        csv_writer.writerow(self.joiner(self.process_record(record=marc_record)))
+                        processed += 1
+                        counter += 1
                         marc_record.clear()
-                        marc_record.append(line)
-                    else:
-                        marc_record.append(line)
 
-                self.process_record(record=marc_record, o_file=o_file)
+
+                    if counter >= self.step:
+                        counter = 0
+                        print("{} of {} records processed".format(processed, amount_of_records))
+
+                    marc_record.append(line)
+
+                csv_writer.writerow(self.joiner(self.process_record(record=marc_record)))
+                processed += 1
+                print("{} of {} records processed".format(processed, amount_of_records))
+
--- converter.py
+++ converter.py
@@ -1,18 +1,16 @@
 import argparse
 
-
 from FilesManager import FilesManager
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", help = "Path to configuration file .ini.", required = True)
+parser.add_argument("--config", help="Path to configuration file .ini.", required=True)
 parser.add_argument("--input", help="Path to input .mrc file.", required=True)
 parser.add_argument("--output", help="Path to output .csv file.", required=True)
 parser.add_argument("--separator", help="Separator string.", default="$|$")
+parser.add_argument("--step", help="Step for statistics.", default=100)
 args = parser.parse_args()
 
-
-
 if __name__ == "__main__":
-
-    filesManager = FilesManager.FilesManager(input_file=args.input, output_file=args.output, config_file=args.config, separator=args.separator)
+    filesManager = FilesManager.FilesManager(input_file=args.input, output_file=args.output, config_file=args.config,
+                                             separator=args.separator, step=args.step)
     filesManager.convert()
