e0d030113030f2483e7c80ab0c7d519da8d1ca69
SergeyPanov <panovsy@gmail.com>
2017-06-23 13:28:06 +0200

    Added configuration file

--- FilesManager/FilesManager.py
+++ FilesManager/FilesManager.py
@@ -1,6 +1,20 @@
 import os
 import sys
 import re
+import csv
+import configparser
+
+
+def check_file(pathToFile, extension):
+    if os.path.exists(pathToFile):
+        if pathToFile.endswith(extension):
+            return True
+        else:
+            print("File has wrong extension, expected {}".format(extension), file=sys.stderr)
+            raise IOError
+    else:
+        print("File {} is not found".format(pathToFile), file=sys.stderr)
+        raise FileNotFoundError
 
 
 class FilesManager:
@@ -8,60 +22,61 @@ class FilesManager:
     Class is managing input and output files.
     """
 
-    def __init__(self, input_file, output_file, fields, all):
+    def __init__(self, input_file, output_file, config_file, separator):
+        check_file(input_file, ".mrc")
+        check_file(config_file, ".ini")
+
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+        self.fields = dict(self.config["FIELDS"].items())
         self.input_file = input_file
         self.output_file = output_file
-        self.fields = fields
-        self.all = all
-        self.check_file(self.input_file, ".mrc")
+        self.separator = separator
 
-    def check_file(self, pathToFile, extension):
-        if os.path.exists(pathToFile):
-            if pathToFile.endswith(extension):
-                return True
-            else:
-                print("File has wrong extension, expected {}".format(extension), file=sys.stderr)
-                raise IOError
-        else:
-            print("File {} is not found".format(pathToFile), file=sys.stderr)
-            raise FileNotFoundError
+
 
     def process_record(self, record, o_file):
         processed = {}
-        clearLambda = lambda lin: re.sub("\$\S?", "", lin)
+        clear_lambda = lambda lin: re.sub("\$\S?", "", lin)
         for line in record:
             line = re.sub("\n", "", line)
             field = ""
             if len(line) > 0:
                 field = line.split()[0]
-            if field in self.fields:
+            if field in list(dict(self.fields.items()).keys()):
                 if line.startswith(field):
-                    if self.all:
+
+                    for subfield_tag in str(self.fields[field]).split():
+
+                        vals = list(map(lambda l: re.sub("\$\S?", "", l), list(filter(lambda s: len(s) > 0, list(map(lambda subfield: subfield if str(subfield).startswith(subfield_tag) else "",
+                                 re.findall("\$\S?[^\$\n]*", line)))))))
+
                         if field in processed:
-                            processed[field] += list(map(clearLambda, re.findall("\$\S?[^\$\n]*", line)))
+                            processed[field] += vals
                         else:
-                            processed[field] = list(map(clearLambda, re.findall("\$\S?[^\$\n]*", line)))
-                    else:
-                        if field in processed:
-                            processed[field] += list(
-                                map(lambda lin: re.sub("\$\S?", "", lin), [re.search("\$\S?[^\$\n]*", line).group()]))
-                        else:
-                            processed[field] = list(
-                                map(lambda lin: re.sub("\$\S?", "", lin), [re.search("\$\S?[^\$\n]*", line).group()]))
+                            processed[field] = vals
         return processed
 
-    def convert(self):
+    def joiner(self, record_dict):
+        merged_fields = {}
+        for key, value in dict(record_dict).items():
+            merged_fields[key] = str(self.separator).join(value)
 
+        return merged_fields
+
+    def convert(self):
         with open(self.input_file) as iFile:
 
             with open(self.output_file, "w+") as o_file:
                 marc_record = []
 
+                csv_writer = csv.DictWriter(o_file, fieldnames=self.fields)
+                csv_writer.writeheader()
+
                 for line in iFile:
 
                     if line.startswith("LEADER") and len(marc_record) > 0:
-                        aux = self.process_record(record=marc_record, o_file=o_file)
-                        print(aux)
+                        csv_writer.writerow(self.joiner(self.process_record(record=marc_record, o_file=o_file)))
                         marc_record.clear()
                         marc_record.append(line)
                     else:
--- convertor.py
+++ convertor.py
@@ -1,17 +1,18 @@
 import argparse
 
+
 from FilesManager import FilesManager
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--fields", help="Defines columns names of result CSV files", nargs="+", required=True)
-parser.add_argument("--all", help="If parameter is turned on, all separated parts of fields will be used,"
-                                  " otherwise only first.", action="store_true")
+parser.add_argument("--config", help = "Path to configuration file .ini.", required = True)
 parser.add_argument("--input", help="Path to input .mrc file.", required=True)
 parser.add_argument("--output", help="Path to output .csv file.", required=True)
+parser.add_argument("--separator", help="Separator string.", default="$|$")
 args = parser.parse_args()
 
 
 
 if __name__ == "__main__":
-    filesManager = FilesManager.FilesManager(input_file=args.input, output_file=args.output, fields=args.fields, all=args.all)
+
+    filesManager = FilesManager.FilesManager(input_file=args.input, output_file=args.output, config_file=args.config, separator=args.separator)
     filesManager.convert()
